75ee3cc8c3be000e5bee4b737169bde73d9bfb66
Потапов Роман <potapov@roman>
2024-03-05 19:56:34 +0300

    Создал цикл и определил длину mass2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿string[] mass1 = ["Hgggo", "2", "ld", ":-)"];
+int size_mass2 = 0;
 
+for (int i = 0; i < mass1.Length; i++)
+{
+    if (mass1[i].Length <= 3)
+    {
+        size_mass2 = size_mass2 += 1;
+    }
+}
