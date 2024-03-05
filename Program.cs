858fc33ac6c3112617b826a454d12d30795e051d
Потапов Роман <potapov@roman>
2024-03-05 19:58:24 +0300

    Создал второй цикл который находит нужные элементы массива и вывел на экран результат

--- Program.cs
+++ Program.cs
@@ -8,3 +8,17 @@ for (int i = 0; i < mass1.Length; i++)
         size_mass2 = size_mass2 += 1;
     }
 }
+
+string[] mass2 = new string[size_mass2];
+int j = 0; // счетчик индекса для mass2
+
+for (int i = 0; i < mass1.Length; i++)
+{
+    if (mass1[i].Length <= 3)
+    {
+        mass2[j] = mass1[i];
+        j += 1;
+    }
+}
+
+Console.WriteLine($"Массив: [ {string.Join("; ", mass2)} ]");
